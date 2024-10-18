47ad717ab64c97ac845a9ce69a90e0347a95b74c
Agsergio04 <saragar509@g.educaand.es>
2024-10-18 21:32:02 +0200

    Relacion de ejercicos 21:32 18/10/2024

--- src/ej21_03.py
+++ src/ej21_03.py
@@ -2,11 +2,20 @@ def division_numero(numerador : str,denominador : str) -> float:
     div_resultado = 0
     try:
         div_resultado = float(numerador) / float(denominador)
+    except ValueError:
+        print("**ERROR**\nO el numerador o el denominador no es valido")
     except ZeroDivisionError:
         print("**ERROR**\nNo se puede dividir por 0.")
-    
+        
     return div_resultado
+
 def main():
-    numerador = input("Introduce")
+    #Tengo que crear las variables de la comprobacion del numerador y del denomiador de la hemeroteca
+    numerador = input("Introduce el numerador: ")
+    denominador = input("Introduce el denominador: ")
+    
+    resultado = division_numero(numerador,denominador)
+    
+    print(f"")
 if __name__ == "__main__":
     main()

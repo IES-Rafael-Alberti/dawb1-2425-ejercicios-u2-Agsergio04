698b03f84c5435b7457ac9a09220fde1537334c4
Agsergio04 <saragar509@g.educaand.es>
2024-10-21 21:35:15 +0200

    Relacion de ejercicios 21:35 /21/10/2024

---
+++ src/ej21_04.py
@@ -0,0 +1,11 @@
+def par_o_impar(numero):
+    comprobacion = "Impar" 
+    if (numero % 2 == 0):
+        comprobacion = "par"
+    
+    return comprobacion
+
+def main():
+    pass
+if __name__ == "__main__":
+    main()

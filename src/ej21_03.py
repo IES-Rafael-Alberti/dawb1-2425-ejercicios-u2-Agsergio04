32de7487f9994d04078bab9310219a64ce8b002b
Agsergio04 <saragar509@g.educaand.es>
2024-10-21 21:29:30 +0200

    Relacion de ejercicios 21:29 21/10/2024

--- src/ej21_03.py
+++ src/ej21_03.py
@@ -1,8 +1,7 @@
 def division_numero(primer_numero,segundo_numero):
     resultado  = 0
     
-    numerador = comprobar_numero(primer_numero)
-    denominador = comprobar_numero(segundo_numero)
+    numerador,denominador = primer_numero,segundo_numero
     
     resultado = numerador / denominador
     
@@ -10,8 +9,8 @@ def division_numero(primer_numero,segundo_numero):
 
 def main():
     try:
-        primer_numero = input("Introduce un numero: ")
-        segundo_numero = input("Introduce otro numero: ")
+        primer_numero = int(input("Introduce un numero: "))
+        segundo_numero = int(input("Introduce otro numero: "))
         
         resultado = division_numero(primer_numero,segundo_numero)
     except ValueError:

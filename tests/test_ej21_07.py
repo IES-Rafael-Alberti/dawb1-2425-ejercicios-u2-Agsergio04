8ef41f12d0eb80feb2ac7a2763d7c4c9505b71ad
Agsergio04 <agsergio@g.educaand.es>
2024-11-04 10:40:52 +0100

    Relacion de ejercicios 10:41 04/11/2024

--- tests/test_ej21_07.py
+++ tests/test_ej21_07.py
@@ -1,7 +1,6 @@
 from src.ej21_07 import calculo_tipo_impositivo
 
 def test_calculo_tipo_impositivo(improvisto):
-    assert calculo_tipo_impositivo(-12) == None
     assert calculo_tipo_impositivo(12) == 5
     assert calculo_tipo_impositivo(15000) == 15
     assert calculo_tipo_impositivo(37000) == 35
